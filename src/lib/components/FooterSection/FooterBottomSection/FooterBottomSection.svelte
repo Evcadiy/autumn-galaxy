0def200e1a6ee303f41136cd300fb4454cd61b24
Evcadiy <nikusha1305@gmail.com>
2024-10-16 09:47:42 +0300

    fix: footer fix

--- src/lib/components/FooterSection/FooterBottomSection/FooterBottomSection.svelte
+++ src/lib/components/FooterSection/FooterBottomSection/FooterBottomSection.svelte
@@ -9,7 +9,7 @@
   >
     <!-- Logo Section -->
     <div class="flex items-start justify-center pl-4 md:pr-20 max-md:pb-12">
-      <a href="/"><img src={logo} alt="logo" class="w-16" /></a>
+      <a href="/"><img src={logo} alt="logo" class="w-20" /></a>
     </div>
     <div class="flex flex-1 space-x-16 justify-start">
       <!-- About Section -->
